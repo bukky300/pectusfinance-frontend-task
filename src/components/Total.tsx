2d5b86b038543cc42eb9181b8a830f30ccd3f1fa
bukky300 <bukkybrown11@gmail.com>
2022-07-10 16:40:04 +0100

    more refacoring

--- src/components/Total.tsx
+++ src/components/Total.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import DropDown from "./DropDown";
 import { Expense } from "../types/model";
 import { ExpensesContext } from "../store/expenses-context";
@@ -13,6 +14,7 @@ function Total({ fields, data }: Prop) {
   // dropdown state
   const [value, setValue] = useState("departments");
   const [dropdown, setDropdown] = useState(false);
+  const navigate = useNavigate();
   const options = fields.filter((field) => field !== "amount");
   // add data to context
   useEffect(() => {
@@ -31,18 +33,30 @@ function Total({ fields, data }: Prop) {
       data-testid="totalcontent"
       className="flex flex-col items-center w-full justify-center my-28"
     >
-      <div className="flex w-4/5 justify-end">
-        <p className="mr-4">Total Expenses By:</p>
-        <select
-          className=" px-2 py-1 bg-gray-200 uppercase outline-none"
-          onChange={(e) => setValue(e.target.value)}
-        >
-          {options.map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
-        </select>
+      <div className="flex w-4/5 justify-between">
+        <div>
+          <button
+            type="button"
+            onClick={() => navigate(-1)}
+            className="bg-blue-600 px-4 py-2.5 rounded text-xs text-white
+            hover:bg-blue-700 hover:shadow-lg"
+          >
+            Back
+          </button>
+        </div>
+        <div className="flex">
+          <p className="mr-4">Total Expenses By:</p>
+          <select
+            className=" px-2 py-1 bg-gray-200 uppercase outline-none"
+            onChange={(e) => setValue(e.target.value)}
+          >
+            {options.map((opt) => (
+              <option key={opt} value={opt}>
+                {opt}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       {dropdown && <DropDown category={value} />}
     </div>
--- src/components/util/util.ts
+++ src/components/util/util.ts
@@ -1,24 +1,23 @@
-// @ts-nocheck
-
-const getValues = (category, data) => {
+const getValues = (category: string, data: {}[]) => {
   const values = [];
   // reduce the data array into categories
-  const newData = data.reduce(
-    (cache, expense) => ({
+  const newData: { [key: string]: any } = data.reduce(
+    (cache: { [key: string]: string }, expense: { [key: string]: string }) => ({
       ...cache,
       [expense[category]]:
-        expense[category] in cache
-          ? cache[expense[category]].concat(expense)
-          : [expense],
+        expense[category] in cache ? cache[expense[category]] : [expense],
     }),
     {}
   );
   //  loops thru the categories and adds the individual amounts
   for (const key in newData) {
     if (newData.hasOwnProperty(key)) {
-      const result = newData[key].reduce((acc, obj) => {
-        return acc + Number(obj.amount.replace(/[^0-9.-]+/g, ""));
-      }, 0);
+      const result = newData[key].reduce(
+        (acc: number, obj: { [key: string]: string }) => {
+          return acc + Number(obj.amount.replace(/[^0-9.-]+/g, ""));
+        },
+        0
+      );
       values.push({ key, result });
     }
   }
